2752591b60d63abaf979b35c9f4b7273348f82da
lucianodepalma <lucianodepalma@hotmail.com>
2022-03-09 21:13:18 -0300

    agregando fontawesome al proyecto

--- frontend/src/components/FormularioCompra.js
+++ frontend/src/components/FormularioCompra.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import '../assets/css/FormularioCompra.css';
 
 function Formulario () {
     return (
-        <div style={{margin:'auto'}}>Soy el formulario</div>
+        <div style={{margin:'auto'}}>Soy el formulario <FontAwesomeIcon className="error-icon" icon={faCircleXmark} /> </div>
     )
 }
 export default Formulario;
