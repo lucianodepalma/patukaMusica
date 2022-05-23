541a3a04852c2701d1976e4bad0c19974e83ff8d
lucianodepalma <lucianodepalma@hotmail.com>
2022-05-23 17:52:42 -0300

    modificando precio producto

--- src/components/FormularioCompra.js
+++ src/components/FormularioCompra.js
@@ -23,7 +23,7 @@ function Formulario () {
     const [errorEmail, setErrorEmail] = useState(false);
     const [errorValidEmail, setErrorValidEmail] = useState(false);
     const [fail, setFail] = useState(true);
-    const [precioProducto, setPrecioProducto] = useState(3700);
+    const [precioProducto, setPrecioProducto] = useState(2290);
     const isValidEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const form = useRef();
     const errors = {
