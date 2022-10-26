1b3eaf722d40cf5a192c8980d18985e9f8c18c92
lucianodepalma <lucianodepalma@hotmail.com>
2022-10-26 19:10:47 -0300

    fixeando error de refresh de pagina

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,21 @@ import Descargas from './Descargas';
 import FormularioCompra from './FormularioCompra';
 import PagoAprobado from './PagoAprobado';
 import Header from './Header';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-        <Routes>
-          <Route exact path='/' element={<Libro/>}/>
-          <Route path='/redes' element={<Redes/>}/>
-          <Route path='/descargas' element={<Descargas/>}/>
-          <Route path='/formulario-compra' element={<FormularioCompra/>}/>
-          <Route path='/pago-aprobado' element={ <PagoAprobado/> }/>
-        </Routes>
+      <HashRouter>
+        <Header/>
+          <Routes>
+            <Route exact path='/' element={<Libro/>}/>
+            <Route path='/redes' element={<Redes/>}/>
+            <Route path='/descargas' element={<Descargas/>}/>
+            <Route path='/formulario-compra' element={<FormularioCompra/>}/>
+            <Route path='/pago-aprobado' element={ <PagoAprobado/> }/>
+          </Routes>
+      </HashRouter>
     </div>
   );
 }
